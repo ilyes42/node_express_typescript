64508f00efd4fb8bb7357176b46252cc34833c9e
ihammami <ihammami@alter-solutions.com>
2023-10-30 10:15:15 +0100

    add ApiUserType

--- src/domains/user/models/user.api.model.ts
+++ src/domains/user/models/user.api.model.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const ApiUser = z.object({
+export const ApiUserSchema = z.object({
   username: z.string().min(3).max(14),
   password: z.string().min(8).max(32),
 });
+
+export type ApiUserType = z.infer<typeof ApiUserSchema>;
