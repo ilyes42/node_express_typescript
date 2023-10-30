f588f491e05be4e7f4d2f595198b653f9a021ff7
ihammami <ihammami@alter-solutions.com>
2023-10-30 10:15:39 +0100

    refactoring : populate userService

--- src/domains/user/user.router.ts
+++ src/domains/user/user.router.ts
@@ -1,20 +1,26 @@
 import { Request, Response, Router } from "express";
 import { validateRequest } from "zod-express-middleware";
-import { ApiUser } from "./models/user.api.model";
-
+import { ApiUserSchema } from "./models/user.api.model";
 import DatabaseUser from "./models/user.database.model";
-import DatabaseBook from "../book/models/book.database.model";
+import {
+  createNewUser,
+  getAllUsers,
+  getBooksOfUser,
+  getOneUserById,
+} from "./user.service";
 
 const userRouter: Router = Router();
 
+// Gets all users
 userRouter.get("/", async (request: Request, response: Response) => {
-  const users = await DatabaseUser.findAll();
+  const users = await getAllUsers();
   return response.status(200).json(users);
 });
 
+// Gets 1 user by id
 userRouter.get("/:userId", async (request: Request, response: Response) => {
   const { userId } = request.params;
-  const user = await DatabaseUser.findByPk(userId, { include: [DatabaseBook] });
+  const user = await getOneUserById(userId);
   if (!user) {
     return response
       .status(404)
@@ -23,24 +29,25 @@ userRouter.get("/:userId", async (request: Request, response: Response) => {
   return response.status(200).json(user);
 });
 
+// gets the books of a user
 userRouter.get(
   "/:userId/books",
   async (request: Request, response: Response) => {
     const { userId } = request.params;
-    const userBooks = DatabaseBook.findAll({ where: { userId } });
+    const userBooks = getBooksOfUser(userId);
     return response.status(200).json(userBooks);
   }
 );
 
+// creates a new user
 userRouter.post(
   "/",
   validateRequest({
-    body: ApiUser,
+    body: ApiUserSchema,
   }),
   async (request: Request, response: Response) => {
     const { username, password } = request.body;
-    const newUser: DatabaseUser = new DatabaseUser({ username, password });
-    await newUser.save();
+    const newUser: DatabaseUser = await createNewUser({ username, password });
     return response.status(201).json(newUser);
   }
 );
--- src/domains/user/user.service.ts
+++ src/domains/user/user.service.ts
@@ -1,7 +1,52 @@
+import DatabaseBook from "../book/models/book.database.model";
 import DatabaseUser from "./models/user.database.model";
+import { type ApiUserType } from "./models/user.api.model";
 
 // Checks whether a user with a certain ID exists or not.
 export async function userExists(userId: number): Promise<boolean> {
   const user = await DatabaseUser.findByPk(userId);
   return user !== null;
+}
+
+export async function getAllUsers(): Promise<DatabaseUser[]> {
+  try {
+    const users = await DatabaseUser.findAll();
+    return users;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getOneUserById(
+  userId: string
+): Promise<DatabaseUser | null> {
+  try {
+    const user = await DatabaseUser.findByPk(userId, {
+      include: [DatabaseBook],
+    });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getBooksOfUser(userId: string): Promise<DatabaseBook[]> {
+  try {
+    const userBooks = DatabaseBook.findAll({ where: { userId } });
+    return userBooks;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createNewUser(
+  apiUser: ApiUserType
+): Promise<DatabaseUser> {
+  try {
+    const newUser: DatabaseUser = new DatabaseUser(apiUser);
+    await newUser.save();
+    return newUser;
+  } catch (error) {
+    throw error;
+  }
 }
